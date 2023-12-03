b548b4df9984e8e8e563d4bbf6e82d8409218faf
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-12-03 11:28:41 +0600

    Installing eslint, refactor code, fix errors using command

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-const express = require("express");
-const app = express();
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-console.log("h");
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-// all connectivity is here
-const mongoose = require("mongoose");
+import app from "./app";
+import config from "./app/config";
+
+import mongoose from "mongoose";
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL);
+  try {
+    await mongoose.connect(config.database_url as string);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
