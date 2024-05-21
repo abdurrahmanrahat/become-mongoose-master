ec4dd8093583767474c0c6455f429a120b769a08
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-21 20:55:17 +0600

    academic faculty controller function

--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -26,7 +26,7 @@ const updateAcademicFacultyIntoDB = async (
   return result;
 };
 
-export const AcademicSemesterServices = {
+export const AcademicFacultyServices = {
   createAcademicFacultyIntoDb,
   getAllAcademicFacultiesFromDB,
   getSingleAcademicFacultyFromDB,
