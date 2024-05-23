79c8ed5391d38c9cc55cc289d280a439e6c0a705
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-23 22:05:35 +0600

    academic department route

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import ValidateRequest from '../../middlewares/ValidateRequest';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  ValidateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  ValidateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
