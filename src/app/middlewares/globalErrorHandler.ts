562c8a4080caedcdf007d76e4c6cda1a2d442ff9
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-27 15:56:15 +0600

    handleCastError

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,6 +4,7 @@
 import { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 import config from '../config';
+import { handleCastError } from '../errors/handleCastError';
 import { handleValidationError } from '../errors/handleValidationError';
 import { handleZodError } from '../errors/handleZodError';
 import { TErrorSource } from '../interface/error';
@@ -27,6 +28,12 @@ export const globalErrHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
 
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
