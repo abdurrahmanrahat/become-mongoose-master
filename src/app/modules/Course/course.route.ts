2a33ea3488325bc28a9258954c21de450b82c4e4
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-30 16:46:49 +0600

    course route

---
+++ src/app/modules/Course/course.route.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import ValidateRequest from '../../middlewares/ValidateRequest';
+import { CourseControllers } from './course.controller';
+import { CourseValidations } from './course.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-course',
+  ValidateRequest(CourseValidations.createCourseValidationSchema),
+  CourseControllers.createCourse,
+);
+
+router.get('/:id', CourseControllers.getSingleCourse);
+
+router.patch(
+  '/:id',
+  ValidateRequest(CourseValidations.updateCourseValidationSchema),
+  CourseControllers.updateCourse,
+);
+
+router.delete('/:id', CourseControllers.deleteCourse);
+
+router.put(
+  '/:courseId/assign-faculties',
+  ValidateRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.assignFacultiesWithCourse,
+);
+
+router.delete(
+  '/:courseId/remove-faculties',
+  ValidateRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.removeFacultiesFromCourse,
+);
+
+router.get('/', CourseControllers.getAllCourses);
+
+export const CourseRoutes = router;
