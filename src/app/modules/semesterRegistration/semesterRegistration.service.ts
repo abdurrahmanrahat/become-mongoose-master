9d6f3bca5d419e8181d37768f7d814ce8ace3435
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-31 11:39:41 +0600

    createSemesterRegistrationIntoDb

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,12 +1,33 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistration } from './semesterRegistration.model';
 
 const createSemesterRegistrationIntoDb = async (
   payload: TSemesterRegistration,
 ) => {
+  const academicSemester = payload?.academicSemester;
+
   // check if the semester is exist
-  if (payload.academicSemester) {
-    //
+  const isAcademicSemesterExist =
+    await AcademicSemester.findById(academicSemester);
+
+  if (!isAcademicSemesterExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic semester not found');
   }
+
+  // check if the semester is already registered!
+  const isSemesterRegistrationExits = await SemesterRegistration.findOne({
+    academicSemester: academicSemester,
+  });
+
+  if (isSemesterRegistrationExits) {
+    throw new AppError(httpStatus.CONFLICT, 'Semester is already exits');
+  }
+
+  const result = await SemesterRegistration.create(payload);
+  return result;
 };
 
 const getAllSemesterRegistrationsFromDb = async () => {};
