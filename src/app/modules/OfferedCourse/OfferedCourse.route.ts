dd44b60acf5cd2b5a327a78990b61b85145a6429
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-31 21:56:26 +0600

    route

---
+++ src/app/modules/OfferedCourse/OfferedCourse.route.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import ValidateRequest from '../../middlewares/ValidateRequest';
+import { OfferedCourseControllers } from './OfferedCourse.controller';
+import { OfferedCourseValidations } from './OfferedCourse.validation';
+
+const router = express.Router();
+
+router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+
+router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
+
+router.post(
+  '/create-offered-course',
+  ValidateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
+  OfferedCourseControllers.createOfferedCourse,
+);
+
+router.patch(
+  '/:id',
+  ValidateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
+  OfferedCourseControllers.updateOfferedCourse,
+);
+
+router.delete('/:id', OfferedCourseControllers.deleteOfferedCourseFromDB);
+
+export const offeredCourseRoutes = router;
