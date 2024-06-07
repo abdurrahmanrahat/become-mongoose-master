ff00c278f5d24636e30f94c1e4577d71826360bc
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-06-07 22:52:08 +0600

    auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,16 +1,42 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 import AppError from '../errors/AppError';
+import { TUserRole } from '../modules/user/user.interface';
 import catchAsync from '../utils/catchAsync';
 
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
+
+    // check if the token is sent from the client
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized user');
     }
 
-    next();
+    // check if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // err
+        if (err) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized user');
+        }
+
+        // role check
+        const role = (decoded as JwtPayload).role;
+        if (requiredRoles && !requiredRoles.includes(role)) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized user');
+        }
+
+        // decoded undefined
+        req.user = decoded as JwtPayload;
+
+        next();
+      },
+    );
   });
 };
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,26 +1,31 @@
 import express from 'express';
 import ValidateRequest from '../../middlewares/ValidateRequest';
+import auth from '../../middlewares/auth';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
 import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
 import { StudentValidations } from '../student/student.validation';
+import { USER_ROLE } from './user.constant';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   ValidateRequest(StudentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
 
 router.post(
   '/create-faculty',
+  auth(USER_ROLE.admin),
   ValidateRequest(createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
 
 router.post(
   '/create-admin',
+  // auth(USER_ROLE.admin),
   ValidateRequest(createAdminValidationSchema),
   UserControllers.createAdmin,
 );
