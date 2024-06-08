bc551d30bad69db5088172336a3153c82a319a38
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-06-08 09:35:04 +0600

    add gaurd

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import ValidateRequest from '../../middlewares/ValidateRequest';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 import { CourseControllers } from './course.controller';
 import { CourseValidations } from './course.validation';
 
@@ -7,19 +9,25 @@ const router = express.Router();
 
 router.post(
   '/create-course',
+  auth(USER_ROLE.admin),
   ValidateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
-router.get('/:id', CourseControllers.getSingleCourse);
+router.get(
+  '/:id',
+  auth('admin', 'faculty', 'student'),
+  CourseControllers.getSingleCourse,
+);
 
 router.patch(
   '/:id',
+  auth('admin'),
   ValidateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
 
-router.delete('/:id', CourseControllers.deleteCourse);
+router.delete('/:id', auth('admin'), CourseControllers.deleteCourse);
 
 router.put(
   '/:courseId/assign-faculties',
